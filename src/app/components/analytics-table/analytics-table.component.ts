ed0927117ec49d5b37aff10c1dde11307c956301
Traven <TRAVEN.BLEVINS9061@STU.MTEC.EDU>
2025-03-26 09:17:46 -0600

    unit 2 complete

---
+++ src/app/components/analytics-table/analytics-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analytics-table',
+  standalone: false,
+  templateUrl: './analytics-table.component.html',
+  styleUrl: './analytics-table.component.css'
+})
+export class AnalyticsTableComponent {
+
+}
---
+++ src/app/components/top-navbar/top-navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-navbar',
+  standalone: false,
+  templateUrl: './top-navbar.component.html',
+  styleUrl: './top-navbar.component.css'
+})
+export class TopNavbarComponent {
+
+}
