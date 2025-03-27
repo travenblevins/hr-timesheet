2c50ff3e52fb87b6397b48b1212ea45ae3231ef7
Traven <TRAVEN.BLEVINS9061@STU.MTEC.EDU>
2025-03-27 09:22:08 -0600

    unit 6 complete

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DepartmentsService } from '../../services/departments.service';
 import { Department } from '../../interfaces/department';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-departments',
@@ -12,8 +13,12 @@ export class DepartmentsComponent {
   departments: Department[];
   constructor(
     private departmentsService: DepartmentsService,
+    private router: Router
   ) { }
   ngOnInit(): void {
     this.departments = this.departmentsService.departments;
   }
+  goToDepartment(departmentId: string): void {
+    this.router.navigate(['./timesheet', {id: departmentId}]);
+}
 }
--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Department } from '../../interfaces/department';
+import { DepartmentsService } from '../../services/departments.service';
 
 @Component({
   selector: 'app-timesheet',
@@ -7,5 +10,15 @@ import { Component } from '@angular/core';
   styleUrl: './timesheet.component.css'
 })
 export class TimesheetComponent {
+  departments: Department[];
+  department: Department;
+  constructor(
+    private route: ActivatedRoute,
+    private DepartmentsService: DepartmentsService
+  ) { }
 
+  ngOnInit(): void {
+    this.departments = this.DepartmentsService.departments;
+    this.department = this.departments.find(department => department.id === this.route.snapshot.params['id']);
+  }
 }
