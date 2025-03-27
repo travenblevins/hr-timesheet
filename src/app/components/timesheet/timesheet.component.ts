5ffbaa947bd5ce8dbada6840796854f1f4b91e20
Traven <TRAVEN.BLEVINS9061@STU.MTEC.EDU>
2025-03-27 10:08:30 -0600

    unit 7 complete

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Department } from '../../interfaces/department';
 import { DepartmentsService } from '../../services/departments.service';
+import { Employee } from '../../interfaces/employee';
+import { FormControl, AbstractControl, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-timesheet',
@@ -12,6 +14,9 @@ import { DepartmentsService } from '../../services/departments.service';
 export class TimesheetComponent {
   departments: Department[];
   department: Department;
+  employeeNameFC = new FormControl('', this.nameValidator());
+  employees: Employee[] = [];
+  employeeId = 0;
   constructor(
     private route: ActivatedRoute,
     private DepartmentsService: DepartmentsService
@@ -21,4 +26,31 @@ export class TimesheetComponent {
     this.departments = this.DepartmentsService.departments;
     this.department = this.departments.find(department => department.id === this.route.snapshot.params['id']);
   }
+  addEmployee(): void {
+    if (this.employeeNameFC.value) {
+      this.employeeId++;
+
+      this.employees.push({
+        id: this.employeeId.toString(),
+        departmentId: this.department?.id,
+        name: this.employeeNameFC.value,
+        payRate: Math.floor(Math.random() * 50) + 50,
+      });
+
+      this.employeeNameFC.setValue('');
+    }
+  }
+  nameValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+        let error = null;
+        if (this.employees && this.employees.length) {
+            this.employees.forEach(employee => {
+                if (employee.name.toLowerCase() === control.value.toLowerCase()) {
+                    error = {duplicate: true};
+                }
+            });
+        }
+        return error;
+    };
+}
 }
--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -5,15 +5,18 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule, MatToolbarModule, MatButtonModule, MatIconModule, MatCardModule, MatListModule
+    CommonModule, MatToolbarModule, MatButtonModule, MatIconModule, MatCardModule, MatListModule, MatInputModule, MatFormFieldModule, ReactiveFormsModule
   ],
   exports: [
-    MatToolbarModule, MatButtonModule, MatIconModule, MatCardModule, MatListModule
+    MatToolbarModule, MatButtonModule, MatIconModule, MatCardModule, MatListModule, MatInputModule, MatFormFieldModule, ReactiveFormsModule
   ]
 })
 export class MaterialModule { }
