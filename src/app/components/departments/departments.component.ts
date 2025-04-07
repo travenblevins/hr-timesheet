ee500bcbe34fef098b453e21f539638b32eb47b5
Traven <TRAVEN.BLEVINS9061@STU.MTEC.EDU>
2025-04-07 09:21:46 -0600

    unit 12 complete

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -2,25 +2,28 @@ import { Component } from '@angular/core';
 import { DepartmentsService } from '../../services/departments.service';
 import { Department } from '../../interfaces/department';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-departments',
   standalone: false,
   templateUrl: './departments.component.html',
-  styleUrl: './departments.component.css'
+  styleUrl: './departments.component.css',
 })
 export class DepartmentsComponent {
-  departments: Department[];
+  departments: Department[] | undefined;
+  $departments: Observable<Department[]> | undefined;
   constructor(
     private departmentsService: DepartmentsService,
     private router: Router
-  ) { }
+  ) {}
   ngOnInit(): void {
-    this.departmentsService.getDepartments().subscribe(departments => {
-      this.departments = departments;
-  });
+    // this.departmentsService.getDepartments().subscribe((departments) => {
+    //   this.departments = departments;
+    // });
+    this.$departments = this.departmentsService.getDepartments();
   }
   goToDepartment(departmentId: string): void {
-    this.router.navigate(['./timesheet', {id: departmentId}]);
-}
+    this.router.navigate(['./timesheet', { id: departmentId }]);
+  }
 }
